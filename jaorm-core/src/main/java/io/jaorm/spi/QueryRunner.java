2ca8b9f13212bbd7ff9c302173a9ecac822f5911
Donato Leone <ulissis1996@gmail.com>
2021-04-22 10:29:01 +0200

    Update to 0.7.0

--- jaorm-core/src/main/java/io/jaorm/spi/QueryRunner.java
+++ jaorm-core/src/main/java/io/jaorm/spi/QueryRunner.java
@@ -14,7 +14,6 @@ import io.jaorm.spi.common.Singleton;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.*;
 import java.util.stream.Stream;
