d700ec26b1482ce840ce4a6c6f7ac46fdbc2cbdc
Donato Leone <ulissis1996@gmail.com>
2021-07-09 17:58:06 +0200

    Fix sonar complains

--- jaorm/src/main/java/io/github/ulisse1996/jaorm/SimpleQueryRunner.java
+++ jaorm/src/main/java/io/github/ulisse1996/jaorm/SimpleQueryRunner.java
@@ -15,7 +15,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
